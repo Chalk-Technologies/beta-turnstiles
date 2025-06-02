535b630079005a7aa10db3f0c7d7b390654fb662
Miura Hawkins <miura.hawkins@gmail.com>
2025-06-02 17:12:56 +0200

    use evdev

--- turnstiles.py
+++ turnstiles.py
@@ -23,6 +23,8 @@ import threading
 import configparser
 import os
 from typing import Optional
+import evdev
+from evdev import *
 
 # Configuration
 CONFIG_FILE = "config.ini"  # Configuration file path
@@ -33,6 +35,26 @@ API_TIMEOUT = 5.0  # API request timeout in seconds
 RELAY_ACTIVE_TIME = 0.2  # How long to keep relay active (seconds)
 
 DEFAULT_DEVICE = "/dev/input/event0"
+
+scancodes = {
+    # Scancode: ASCIICode
+    0: None, 1: u'ESC', 2: u'1', 3: u'2', 4: u'3', 5: u'4', 6: u'5', 7: u'6', 8: u'7', 9: u'8',
+    10: u'9', 11: u'0', 12: u'-', 13: u'=', 14: u'BKSP', 15: u'TAB', 16: u'q', 17: u'w', 18: u'e', 19: u'r',
+    20: u't', 21: u'y', 22: u'u', 23: u'i', 24: u'o', 25: u'p', 26: u'[', 27: u']', 28: u'CRLF', 29: u'LCTRL',
+    30: u'a', 31: u's', 32: u'd', 33: u'f', 34: u'g', 35: u'h', 36: u'j', 37: u'k', 38: u'l', 39: u';',
+    40: u'"', 41: u'`', 42: u'LSHFT', 43: u'\\', 44: u'z', 45: u'x', 46: u'c', 47: u'v', 48: u'b', 49: u'n',
+    50: u'm', 51: u',', 52: u'.', 53: u'/', 54: u'RSHFT', 56: u'LALT', 57: u' ', 100: u'RALT'
+}
+
+capscodes = {
+    0: None, 1: u'ESC', 2: u'!', 3: u'@', 4: u'#', 5: u'$', 6: u'%', 7: u'^', 8: u'&', 9: u'*',
+    10: u'(', 11: u')', 12: u'_', 13: u'+', 14: u'BKSP', 15: u'TAB', 16: u'Q', 17: u'W', 18: u'E', 19: u'R',
+    20: u'T', 21: u'Y', 22: u'U', 23: u'I', 24: u'O', 25: u'P', 26: u'{', 27: u'}', 28: u'CRLF', 29: u'LCTRL',
+    30: u'A', 31: u'S', 32: u'D', 33: u'F', 34: u'G', 35: u'H', 36: u'J', 37: u'K', 38: u'L', 39: u':',
+    40: u'\'', 41: u'~', 42: u'LSHFT', 43: u'|', 44: u'Z', 45: u'X', 46: u'C', 47: u'V', 48: u'B', 49: u'N',
+    50: u'M', 51: u'<', 52: u'>', 53: u'?', 54: u'RSHFT', 56: u'LALT', 57: u' ', 100: u'RALT'
+}
+
 
 class InputValidator:
     def __init__(self, relay_pin: int = DEFAULT_RELAY_PIN, api_url: str = DEFAULT_API_ENDPOINT, config_file: str = CONFIG_FILE):
@@ -97,7 +119,9 @@ class InputValidator:
                 try:
                     device = config['Keyboard'].get('device').strip()
                     if device:
-                        self.keyboard_device = open(device, 'rb')
+                        self.keyboard_device = evdev.InputDevice(device)
+                        self.keyboard_device.grab()
+#                         self.keyboard_device = open(device, 'rb')
                 except (ValueError, TypeError):
                     pass
 
@@ -259,16 +283,37 @@ class InputValidator:
                         user_input = input("Enter code: ").strip()
                     else:
                         # Make a loop to read binary strings
-                        done = False
-                        while not done:
-                            newChar = self.keyboard_device.read(8).decode("utf-8")
-                            print("got char ")
-                            print(newChar)
-                            print(ord(newChar))
-                            if int(ord(newChar)) == 40:
-                                done = True
-                            else:
-                                user_input += newChar
+                        #loop
+                        caps = False
+                        for event in self.keyboard_device.read_loop():
+                            print(event)
+                            if event.type == ecodes.EV_KEY:
+                                data = categorize(event) # Save the event temporarily to introspect it
+                                if data.scancode == 42:
+                                    if data.keystate == 1:
+                                        caps = True
+                                    if data.keystate == 0:
+                                        caps = False
+                                if data.keystate == 1: # Down events only
+                                    if caps:
+                                        key_lookup = u'{}'.format(capscodes.get(data.scancode)) or u'UNKNOWN:[{}]'.format(data.scancode) # Lookup or return UNKNOWN:XX
+                                    else:
+                                        key_lookup = u'{}'.format(scancodes.get(data.scancode)) or u'UNKNOWN:[{}]'.format(data.scancode) # Lookup or return UNKNOWN:XX
+                                    if (data.scancode != 42) and (data.scancode != 28):
+                                        user_input += key_lookup
+                                    if(data.scancode == 28):
+                                        print(user_input)   # Print it all out!
+                                        break
+#                         done = False
+#                         while not done:
+#                             newChar = self.keyboard_device.read(8).decode("utf-8")
+#                             print("got char ")
+#                             print(newChar)
+#                             print(ord(newChar))
+#                             if int(ord(newChar)) == 40:
+#                                 done = True
+#                             else:
+#                                 user_input += newChar
 #                         user_input = self.keyboard_device.readline()
 
                     # Check for exit commands
